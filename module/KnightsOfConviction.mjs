e29f7144921cad9ae49d6885c550b46a9a753b82
JustVasiliy <vasilyrd2001@gmail.com>
2024-06-17 23:14:42 +0300

    Second setup commit: added config file

--- module/KnightsOfConviction.mjs
+++ module/KnightsOfConviction.mjs
@@ -6,12 +6,14 @@ Hooks.once("init", function () {
     console.log("KnightsOfConviction | init system");
 
     CONFIG.KNIGHTS_OF_CONVICTION = KNIGHTS_OF_CONVICTION;
-    // CONFIG.statusEffects = CONFIG.WITCHER.statusEffects;
+    // эффекты которые можно наложить на токен
+    CONFIG.statusEffects = CONFIG.KNIGHTS_OF_CONVICTION.statusEffects;
     CONFIG.Item.documentClass = KoCItem;
     CONFIG.Actor.documentClass = KoCActor;
 
-    // registerDataModels();
-    // registerSheets();
+    // TO DO: нужно сделать перерегистрацию моделей, пока это файлы-пустышки
+    registerDataModels();
+    registerSheets();
     // preloadHandlebarsTemplates();
     // registerSettings();
 });
--- module/setup/config.js
+++ module/setup/config.js
@@ -1,1 +1,54 @@
 export const KNIGHTS_OF_CONVICTION = {};
+
+KNIGHTS_OF_CONVICTION.attributesTypes = {
+    none: "",
+    int: "ИНТ",
+    dex: "ЛВК",
+    wil: "ВОЛЯ",
+    con: "ТЕЛ",
+    per: "ВОСП"
+}
+
+KNIGHTS_OF_CONVICTION.subAttributesType = {
+    hp: "ПЗ",
+    speed: "Скорость",
+    agile: "Проворство",
+    savvy: "Смекалка",
+    sight: "Зрение",
+    hearing: "Слух",
+    smell: "Обоняние",
+    touch: "Осязание"
+}
+
+KNIGHTS_OF_CONVICTION.statusEffects = [
+    {
+        id: "lying",
+        label: "Лёжа",
+        icon: "",
+    },
+    {
+        id: "sitting",
+        label: "Сидя",
+        icon: "",
+    },
+    {
+        id: "sneak",
+        label: "Исподтишка",
+        icon: "",
+    },
+    {
+        id: "bleed",
+        label: "Кровотечение",
+        icon: "",
+    },
+    {
+        id: "blinded",
+        label: "Ослеплён",
+        icon: "",
+    },
+    {
+        id: "grappled",
+        label: "Схвачен",
+        icon: "",
+    },
+]
